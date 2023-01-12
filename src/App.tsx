94fe62676f7d0279ef0cf488f3c8f01dc4779a19
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-01-12 20:57:33 +0800

    Added react-reverse-portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
-import { Button, Portal, Typography } from "@mui/material";
-import { useRef, useState } from "react";
+import { Button, Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
-  const containerARef = useRef<HTMLDivElement>(null);
-  const containerBRef = useRef<HTMLDivElement>(null);
-  const [show, setShow] = useState(false);
-
-  const handleShow = () => {
-    setShow(!show);
-  };
-
   return (
     <div>
-      <Button variant="contained" onClick={handleShow}>
-        Switch
-      </Button>
+      <Button variant="contained">Switch</Button>
       <div className="App">
-        <div className="container" ref={containerARef}>
-          <Portal
-            container={show ? containerARef.current : containerBRef.current}
-          >
-            <Typography variant="h3">hello world</Typography>
-          </Portal>
+        <div className="container">
+          <Typography variant="h3">hello world</Typography>
         </div>
-        <div className="container" ref={containerBRef}></div>
+        <div className="container"></div>
       </div>
     </div>
   );
