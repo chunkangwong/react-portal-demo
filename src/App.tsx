305bd251b0c72ab470ae6e7a18ea2b3731a800c3
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-01-12 20:45:03 +0800

    Implemented MUI portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import { Typography } from "@mui/material";
+import { Button, Portal, Typography } from "@mui/material";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => {
+    setShow(true);
+  };
+
   return (
-    <div className="App">
-      <div className="container">
-        <Typography variant="h3">hello world</Typography>
+    <div>
+      <Button variant="contained" onClick={handleShow}>
+        Show
+      </Button>
+      <div className="App">
+        <div className="container">
+          {show && (
+            <Portal container={containerRef.current}>
+              <Typography variant="h3">hello world</Typography>
+            </Portal>
+          )}
+        </div>
+        <div className="container" ref={containerRef}></div>
       </div>
-      <div className="container"></div>
     </div>
   );
 }
