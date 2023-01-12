fce1715d994dd03d2622c6633f97467f0f3442f2
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-01-12 21:10:52 +0800

    Implemented containerNode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import { Button, Typography } from "@mui/material";
 import "./App.css";
+import {
+  OutPortal,
+  InPortal,
+  createHtmlPortalNode,
+} from "react-reverse-portal";
+import { useState } from "react";
 
 function App() {
+  const containerANode = createHtmlPortalNode();
+  const containerBNode = createHtmlPortalNode();
+  const [_switch, setSwitch] = useState(false);
+
+  const handleSwitch = () => {
+    setSwitch(!_switch);
+  };
+
   return (
     <div>
-      <Button variant="contained">Switch</Button>
+      <Button variant="contained" onClick={handleSwitch}>
+        Switch
+      </Button>
+      <InPortal node={_switch ? containerANode : containerBNode}>
+        <Typography variant="h3">hello world</Typography>
+      </InPortal>
       <div className="App">
         <div className="container">
-          <Typography variant="h3">hello world</Typography>
+          <OutPortal node={containerANode} />
         </div>
-        <div className="container"></div>
+        <div className="container">
+          <OutPortal node={containerBNode} />
+        </div>
       </div>
     </div>
   );
