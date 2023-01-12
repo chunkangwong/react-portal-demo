77dd6f207ac03fac3bec1688d0a5fe4eb2431121
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-01-12 20:36:16 +0800

    Reverted createPortal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { createPortal } from "react-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        {createPortal(<h1>hello world</h1>, document.body)}
+        <h1>hello world</h1>
       </div>
     </div>
   );
